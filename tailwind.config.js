4ced744f6a2b73ba985c758c91ee59c8f5204b4c
ATEOTG <bryansantos9797@gmail.com>
2023-07-04 14:40:52 -0400

    added custom font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ["Epilogue"],
+      },
+      colors: {
+        primary: {
+          100: "#A060FF",
+          200: "#CB30E3",
+        },
+        secondary: {
+          100: "#FFA84E",
+        },
+        neutral: {
+          100: "#172339",
+          200: "#49566D",
+          300: "#F3EDE7",
+          400: "#FAF8F6",
+        },
+      },
+    },
   },
   plugins: [],
 };
